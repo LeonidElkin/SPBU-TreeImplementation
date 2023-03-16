d8611a30a252c3f22ae388bef614f3b6aec9f636
LeonidElkin <yolkinleon@mail.ru>
2023-03-16 03:14:00 +0300

    Properties return Unit now

--- src/main/kotlin/AvlTree/AvlTree.kt
+++ src/main/kotlin/AvlTree/AvlTree.kt
@@ -6,32 +6,30 @@ import Tree
 class AvlTree<T>(root: AvlVertex<T>): Tree<AvlVertex<T>>(root){
     fun bFactor() = ((this.rightTree as AvlTree<T>?)?.root?.height ?: 0) - ((this.leftTree as AvlTree<T>?)?.root?.height ?: 0)
 
-    fun rotateRight(): AvlTree<T>? {
+    fun rotateRight(){
         val temp = this.leftTree as AvlTree<T>?
         this.leftTree = temp?.rightTree as AvlTree<T>?
         temp?.rightTree = this
         this.updateHeight()
         temp?.updateHeight()
-        return temp
     }
 
-    fun rotateLeft(): AvlTree<T>{
+    fun rotateLeft(){
         val temp = this.rightTree as AvlTree<T>?
         temp?.rightTree = this.leftTree as AvlTree<T>?
         this.leftTree = temp
         temp?.updateHeight()
         this.updateHeight()
-        return this
     }
 
-    fun balance(): AvlTree<T>? {
+    fun balance(){
         this.updateHeight()
         if (this.bFactor() == 2) {
-            if ((this.rightTree as AvlTree<T>?)!!.bFactor() < 0) this.rightTree = (this.rightTree as AvlTree<T>?)?.rotateRight()
+            if ((this.rightTree as AvlTree<T>?)!!.bFactor() < 0) (this.rightTree as AvlTree<T>?)?.rotateRight()
             this.rotateLeft()
         }
         if (this.bFactor() == -2) {
-            if ((this.leftTree as AvlTree<T>?)!!.bFactor() > 0) this.leftTree = (this.leftTree as AvlTree<T>?)?.rotateLeft()
+            if ((this.leftTree as AvlTree<T>?)!!.bFactor() > 0) (this.leftTree as AvlTree<T>?)?.rotateLeft()
             this.rotateRight()
         }
     }
